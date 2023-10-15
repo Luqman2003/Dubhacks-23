0b9d4a2d7ed3d8166accfa2da230421bbd7e69d8
hakshi <lshibly@cs.washington.edu>
2023-10-14 23:56:16 -0700

    Added weekly and monthly logic.

--- BuildingBlocks/Components/DatabaseLogic/MonthlyLogic.js
+++ BuildingBlocks/Components/DatabaseLogic/MonthlyLogic.js
@@ -5,20 +5,22 @@ import { fetchUserScores } from './FetchLogic';
 export default async function updateWeeklyScore(uid) {
   try {
       const response = fetchUserScores(uid);
+      const numWeeks = response.num_weeks;
       const numDaily = response.num_daily;
-      if (numDaily % 4 == 0) {
-        const newNumWeeks = response.num_weeks;
-        const newScore = newNumWeeks + 1;
+      if (numDaily % 4 == 0 || numWeeks % 2 == 0) {
+        const numMonths = response.num_months;
+        const newScore = numMonths + 1;
         const userRef = doc(db, 'users', uid);
         await updateDoc(userRef, {
-            num_weeks: newScore
+            num_months: newScore
         });
         await updateDoc(userRef, {
+            num_weeks: 0,
             num_daily: 0
-        })
-        alert('Your number of weeks was updated successfully!');
+        });
+        alert('Your number of months was updated successfully!');
       } else {
-        alert('You need to do enough dailies before adding a weekly!');
+        alert('You need to do enough weeklies before adding a weekly!');
       }
 
   } catch (err) {
